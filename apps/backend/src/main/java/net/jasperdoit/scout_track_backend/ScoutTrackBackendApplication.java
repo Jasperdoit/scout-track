a2e2cc8364d55a3eb461bf9573fae04015833074
Jasperdoit <jasperdoit@gmail.com>
2026-09-16 12:35:53 +0200

    initial

---
+++ apps/backend/src/main/java/net/jasperdoit/scout_track_backend/ScoutTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package net.jasperdoit.scout_track_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutTrackBackendApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/net/jasperdoit/scout_track_backend/ScoutTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jasperdoit.scout_track_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
